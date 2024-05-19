2ce10f22623c5ae0dbca4c0d6e2063ec09285a2b
LuanKMS <luan.config.js@outlook.com>
2024-05-19 16:13:17 -0300

    CREATED writer router; UPDATED insert nav bar

---
+++ app/writer/page.tsx
@@ -0,0 +1,7 @@
+export default function Writer(){
+  return(
+    <>
+      <h1>writer</h1>
+    </>
+  )
+}
--- components/wrapper/header.tsx
+++ components/wrapper/header.tsx
@@ -3,12 +3,27 @@ import { UserCog, LogOut } from "lucide-react"
 import { toggleTheme } from "../index"
 import { dropMenu, avatar } from "../ui/index"
 import { Logo } from "./logo"
+import Link from "next/link"
+
+function LinkStyled({href, children}: {href: string, children: React.ReactNode}){
+  return(
+    <Link href={href} className="duration-200 hover:opacity-75">{children}</Link>
+  )
+}
 
 export function Header(){
-
   return(
     <header className="sticky top-0 z-50 select-none flex justify-between items-center w-full h-20 p-5 bg-emphasis">
-      <Logo />
+      <div className="flex items-center flex-row gap-7">
+        <Logo />
+
+        <ul className="flex gap-4">
+        <li><LinkStyled href="/">Início</LinkStyled></li>
+          <li><LinkStyled href="/about">Sobre</LinkStyled></li>
+          <li><LinkStyled href="/events">Eventos</LinkStyled></li>
+          <li><LinkStyled href="/writer">Escritores</LinkStyled></li>
+        </ul>
+      </div>
 
       <div className="flex flex-row gap-4 text-sm">
         <toggleTheme.ToggleTheme className="border-none focus-visible:ring-0 focus-visible:ring-offset-0"/>
